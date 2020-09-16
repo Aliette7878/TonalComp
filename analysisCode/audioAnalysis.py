81a82c9065ffa3d5d31b7ec5853730421ed5435f
Jameau Clement <clemjam@hotmail.fr>
2020-09-16 13:29:03 +0200

    Addapting the GUI to the new changes in gui, in particular concerning the plots and the synthesis steps

--- analysisCode/audioAnalysis.py
+++ analysisCode/audioAnalysis.py
@@ -3,11 +3,12 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
+import tkinter as tk
 
-from main import plot_stft, findPeaksScipy, plot_fundamental, findHarmonics_blockMethod, smootherHarmonics, \
+from main import findPeaksScipy, plot_fundamental, findHarmonics_blockMethod, smootherHarmonics, \
     plot_harmonics, build_trajectories, delete_short_trajectories, smooth_trajectories_freq, \
-    plot_trajectories, plot_harmonics_intensity, oscillators_bank_synthesis, \
-    wave_file_creation, resynthesis, custom_synthesis, N_moving_median, minTrajDuration
+    plot_trajectories, plot_harmonics_intensity, plot_synthesis, \
+    wave_file_creation, resynthesis, custom_synthesis, N_moving_median, minTrajDuration, sustain_amp
 
 
 class AudioAnalysis:
@@ -37,7 +38,6 @@ class AudioAnalysis:
             hop_length=self.hop_length, )
         )
         self.X_db = librosa.amplitude_to_db(self.X, ref=np.max)
-        plot_stft(self.X_db, self.Fs)
 
         print("Peak resolution d_f = " + str(analysisParams.d_f) + " Hz")
         print("N_fft = " + str(self.N_fft))
@@ -49,6 +49,7 @@ class AudioAnalysis:
         self.fundThroughFrame = None
         self.fundThroughFrameSmoother = None
         self.Harmonic_freqSmoother = None
+        self.Harmonic_db = None
         self.Harmonic_db_filtered = None
         self.Harmonic_freqMedian = None
         self.trajectories = None
@@ -76,16 +77,15 @@ class AudioAnalysis:
             self.fundThroughFrame = np.zeros(self.n_frames)
             for m in range(self.n_frames):
                 self.fundThroughFrame[m] = \
-                findPeaksScipy(self.X_db[:, m], self.analysisParams.f_low / self.indexToFreq)[0]
+                    findPeaksScipy(self.X_db[:, m], self.analysisParams.f_low / self.indexToFreq)[0]
             self.fundThroughFrameSmoother = scipy.signal.medfilt(self.fundThroughFrame, N_moving_median)
 
         # compute harmonics
-        Harmonic_freq, Harmonic_db = findHarmonics_blockMethod(self.X_db, self.fundThroughFrameSmoother,
+        Harmonic_freq, self.Harmonic_db = findHarmonics_blockMethod(self.X_db, self.fundThroughFrameSmoother,
                                                                self.indexToFreq, False)
         self.Harmonic_freqSmoother = smootherHarmonics(Harmonic_freq, N_moving_median)
 
         plot_harmonics(Harmonic_freq, self.Harmonic_freqSmoother)
-
 
     def show_trajectories(self):
 
@@ -96,15 +96,14 @@ class AudioAnalysis:
                 self.fundThroughFrame[m] = findPeaksScipy(self.X_db[:, m], self.analysisParams.f_low / self.indexToFreq)[0]
             self.fundThroughFrameSmoother = scipy.signal.medfilt(self.fundThroughFrame, N_moving_median)
 
-        #compute harmonics
-        Harmonic_freq, Harmonic_db = findHarmonics_blockMethod(self.X_db, self.fundThroughFrameSmoother, self.indexToFreq, False)
+        # compute harmonics
+        Harmonic_freq, self.Harmonic_db = findHarmonics_blockMethod(self.X_db, self.fundThroughFrameSmoother, self.indexToFreq, False)
         self.Harmonic_freqSmoother = smootherHarmonics(Harmonic_freq, N_moving_median)
 
-
-        minAmp_db = np.min(Harmonic_db)
+        minAmp_db = np.min(self.Harmonic_db)
 
         # Computing trajectories
-        self.trajectories, trajectories_freq, trajectories_db = build_trajectories(Harmonic_db,
+        self.trajectories, trajectories_freq, trajectories_db = build_trajectories(self.Harmonic_db,
                                                                                    self.Harmonic_freqSmoother)
 
         # Plot the found trajectories
@@ -112,14 +111,14 @@ class AudioAnalysis:
 
         # Deleting short trajectories (below specific minimum duration)
         self.trajectories, trajectories_freq, trajectories_db, self.Harmonic_db_filtered = \
-            delete_short_trajectories(self.trajectories, trajectories_freq, trajectories_db, Harmonic_db,
+            delete_short_trajectories(self.trajectories, trajectories_freq, trajectories_db, self.Harmonic_db,
                                       minTrajDuration, minAmp_db)
 
         # Plot the smoothened trajectories, with intensity
         min_y, max_y = np.min(Harmonic_freq), np.max(Harmonic_freq)
         title_traj = "Fundamental and its harmonics after deleting short trajectories"
 
-        plot_harmonics_intensity(self.Harmonic_freqSmoother, Harmonic_db, trajectories_freq, trajectories_db,
+        plot_harmonics_intensity(self.Harmonic_freqSmoother, self.Harmonic_db, trajectories_freq, trajectories_db,
                                  title_traj, min_y, max_y)
 
         # Smoothening out frequency variation within each trajectory with Median
@@ -129,19 +128,37 @@ class AudioAnalysis:
         # Harmonic_freqSmoother should be used for re-synthesis
         # Harmonic_freq_Median should be used for custom_synthesis
 
-
-    def resynthesize(self, file_path):
+    def resynthesize(self):
         if self.Harmonic_db_filtered is None:
             self.show_trajectories()
-        harm_amp = librosa.db_to_amplitude(self.Harmonic_db_filtered)
-        bankosc = resynthesis(harm_amp, self.Harmonic_freqSmoother, self.Fs, self.hop_length, file_path)
+        bankosc_resynth = resynthesis(self.Harmonic_db_filtered, self.Harmonic_freqSmoother, self.Fs, self.hop_length)
 
+        # Plotting the original and the re-synthesised audio files in time domain
+        plot_synthesis(self.audio, bankosc_resynth, "Re-synthesized audio file")
 
-    def customSynth(self, amplitude_array, inharmonicity_array, attack, decay, path_name):
+        f = tk.filedialog.asksaveasfile(initialfile="resynthesisedSound", mode='w', defaultextension=".wav")
+        path_name = f.name
+        f.close()
+        print(path_name)
+
+        # Writing the file with controlled harmonics
+        wave_file_creation(bankosc_resynth, self.Fs, path_name)
+
+    def customSynth(self, amplitude_array, inharmonicity_array, attack, decay):
         if self.Harmonic_freqMedian is None:
             self.show_trajectories()
-        custom_synthesis(self.Harmonic_db_filtered, self.Harmonic_freqMedian, self.trajectories, amplitude_array,
-                                   inharmonicity_array, attack, decay, 0.1, self.Fs, self.hop_length, True, path_name)
+        bankosc_custom_synth = custom_synthesis(self.Harmonic_db_filtered, self.Harmonic_freqMedian, self.Harmonic_db,
+                                                self.Harmonic_freqSmoother, self.trajectories, amplitude_array, inharmonicity_array,
+                                                attack, decay, sustain_amp, self.Fs, self.hop_length, True)
+
+        # Plotting the original and customarily synthesised audio files in time domain
+        plot_synthesis(self.audio, bankosc_custom_synth, "Customarily synthesized audio file")
+        # Writing the file with controlled harmonics
+        f = tk.filedialog.asksaveasfile(initialfile="customSynthesisedSound", mode='w', defaultextension=".wav")
+        path_name = f.name
+        f.close()
+        print(path_name)
+        wave_file_creation(bankosc_custom_synth, self.Fs, path_name)
 
 
 class AnalysisParameters:
